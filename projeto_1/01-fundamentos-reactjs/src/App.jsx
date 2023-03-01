b7400c213140e8f6ff6de75c672ab812b8af3184
Rafael Simionato <rafaasimi@hotmail.com>
2023-03-01 19:48:43 -0300

    Finalização do componente de Sidebar

--- projeto_1/01-fundamentos-reactjs/src/App.jsx
+++ projeto_1/01-fundamentos-reactjs/src/App.jsx
@@ -1,20 +1,27 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
+
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <>
-    <Header />
-      <h1>Hello World</h1>
-      <Post
-        author="Rafael Simionato"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere reprehenderit quia cum voluptatem provident officiis."
-      />
-      <Post
-        author="Diego Fernandes"
-        content="Novo post adicionado. Muito legal."
-      />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Rafael Simionato"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere reprehenderit quia cum voluptatem provident officiis."
+          />
+          <Post
+            author="Diego Fernandes"
+            content="Novo post adicionado. Muito legal."
+          />
+        </main>
+      </div>
     </>
   );
 }
---
+++ projeto_1/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { PencilLine } from "phosphor-react";
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1548588681-adf41d474533?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bW9udGFpbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/rafaasimi.png" alt="Rafael Simionato" />
+        <strong>Rafael Simionato</strong>
+        <span>Engenheiro de Software</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
