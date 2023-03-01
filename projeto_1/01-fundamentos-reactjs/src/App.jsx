e0fa778d0cc7cd67282261a88fae39ee3b31490b
Rafael Simionato <rafaasimi@hotmail.com>
2023-03-01 13:39:23 -0300

    Criado estrutura inicial Header e CSS global

--- projeto_1/01-fundamentos-reactjs/src/App.jsx
+++ projeto_1/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+import './global.css'
 
 export function App() {
   return (
     <>
+    <Header />
       <h1>Hello World</h1>
       <Post
         author="Rafael Simionato"
