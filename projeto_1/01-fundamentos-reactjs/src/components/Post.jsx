c8bd9422363ba340115685c839dafe2d059fcd60
Rafael Simionato <rafaasimi@hotmail.com>
2023-03-02 08:35:03 -0300

    Finalização estilização do componente Post

--- projeto_1/01-fundamentos-reactjs/src/components/Post.jsx
+++ projeto_1/01-fundamentos-reactjs/src/components/Post.jsx
@@ -28,7 +28,16 @@ export function Post(props) {
               <a href="">#nlw</a>{' '}
               <a href="">#rocketseat</a>
                 </p>
-            </div>
+            </div> 
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea placeholder='Deixe um comentário'/>
+
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>            
+            </form>
         </article>
     )
 }
