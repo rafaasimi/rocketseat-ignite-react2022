feaa0c08ae9d74e3bfc66ab172fb8f2598b96738
Rafael Simionato <rafaasimi@hotmail.com>
2023-03-03 10:59:09 -0300

    Criado objeto de posts e populando em tela

--- projeto_1/01-fundamentos-reactjs/src/App.jsx
+++ projeto_1/01-fundamentos-reactjs/src/App.jsx
@@ -5,21 +5,58 @@ import { Sidebar } from './components/Sidebar';
 import './global.css'
 import styles from './App.module.css'
 
+// author: {avatar_url: "", name: "", role: ""}
+// publishedAt: Date
+// content: string
+
 export function App() {
+  const posts = [
+    {
+      id: 1,
+      author: {
+        avatarUrl: 'https://github.com/rafaasimi.png',
+        name: 'Rafael Simionato',
+        role: 'Engenheiro de Software'
+      },
+      content: [
+        {type: 'paragraph', content: 'Fala galeraa 👋'},
+        {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+        {type: 'link', content: 'github.com/rafaasimi'}
+      ],
+      publishedAt: new Date('2023-03-01 09:00:00')
+    },
+    {
+      id: 2,
+      author: {
+        avatarUrl: 'https://github.com/diego3g.png',
+        name: 'Diego Fernandes',
+        role: 'CTO @ Rocketseat'
+      },
+      content: [
+        {type: 'paragraph', content: 'Falaaaa DEV! 👋'},
+        {type: 'paragraph', content: 'Se liga que vem mais uma NLW esse mês'},
+        {type: 'link', content: 'github.com/diego3g'}
+      ],
+      publishedAt: new Date('2023-03-03 09:00:00')
+    }
+  ];
+
   return (
     <>
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Rafael Simionato"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere reprehenderit quia cum voluptatem provident officiis."
-          />
-          <Post
-            author="Diego Fernandes"
-            content="Novo post adicionado. Muito legal."
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
--- projeto_1/01-fundamentos-reactjs/src/components/Post.jsx
+++ projeto_1/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,43 +1,57 @@
-// props: {author: '', content: ''}
+import {format, formatDistanceToNow} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react';
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post(props) {
+export function Post({author, content, publishedAt}) {
+  // Estados => Propriedades que eu quero que o React fique monitorando as mudanças
+  const [comments, setComments] = useState([1,2]);
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
+  function handleCreateNewComment(event) {
+    event.preventDefault();
+    setComments([...comments, comments.length + 1]);
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/maykbrito.png" />
+          <Avatar src={author.avatarUrl} />
 
           <div className={styles.authorInfo}>
-            <strong>Rafael Simionato</strong>
-            <span>Engenheiro de Software</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="01 de Março de 2023" dateTime="2023-03-01 20:00:00">
-          Publicado há 1h
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          <a href=""> jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map(item => {
+          if(item.type === 'paragraph') {
+            return <p>{item.content}</p>
+          } else if (item.type === 'link') {
+            return <p><a href="">{item.content}</a></p>
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea placeholder="Deixe um comentário" />
 
@@ -47,9 +61,7 @@ export function Post(props) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(item => <Comment />)}
       </div>
     </article>
   );
