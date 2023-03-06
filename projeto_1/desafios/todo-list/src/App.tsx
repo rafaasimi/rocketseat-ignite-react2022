34349d346cf6c80eb7310a52cd9cfe569daaad5f
Rafael Simionato <rafaasimi@hotmail.com>
2023-03-05 22:01:05 -0300

    Criado e estilizado componente App

--- projeto_1/desafios/todo-list/src/App.tsx
+++ projeto_1/desafios/todo-list/src/App.tsx
@@ -1,11 +1,18 @@
+
 import { Header } from './components/Header';
+
 import './global.css';
+import styles from './App.module.css';
+import { CreateTaskForm } from './components/CreateTaskForm';
 
 function App() {
 
   return (
     <>
       <Header />
+      <main className={styles.wrapper}>
+        <CreateTaskForm />
+      </main>
     </>
   )
 }
