8662bcf4b219802baa5edb20b23d4a3cb77fa7d1
Rafael Simionato <rafaasimi@hotmail.com>
2023-03-07 20:41:15 -0300

    Configuração do ESLint e ajustado problemas de lint do projeto

--- projeto_2/02-ignite-timer/src/components/Button.styles.ts
+++ projeto_2/02-ignite-timer/src/components/Button.styles.ts
@@ -1,23 +1,26 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from 'styled-components'
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-  variant: ButtonVariant;
+  variant: ButtonVariant
 }
 
 const buttonVariants = {
   primary: 'purple',
   secondary: 'orange',
   danger: 'red',
-  success: 'green'
+  success: 'green',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-background-color: ${props => props.theme['green-500']};
+  background-color: ${(props) => props.theme['green-500']};
 
-  /* ${props => css`background-color: ${buttonVariants[props.variant]}`} */
+  /* ${(props) =>
+    css`
+      background-color: ${buttonVariants[props.variant]};
+    `} */
 `
