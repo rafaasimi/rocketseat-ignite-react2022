e6d947f18f95e1f2486b6d33b98ef62fa1bed78c
Rafael Simionato <rafaasimi@hotmail.com>
2023-03-20 08:25:48 -0300

    Criado rotas, estilos globais e estruturas paginas

--- projeto_2/desafios/coffee-delivery/src/App.tsx
+++ projeto_2/desafios/coffee-delivery/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
-  return <h1>App</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default App
