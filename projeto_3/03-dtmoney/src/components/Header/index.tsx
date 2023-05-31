d594e16813c1feddd10fc501a98221f550b9764b
Rafael Simionato <rafaasimi@hotmail.com>
2023-05-31 18:58:27 -0300

    Add radix lib e criado componente de modal

--- projeto_3/03-dtmoney/src/components/Header/index.tsx
+++ projeto_3/03-dtmoney/src/components/Header/index.tsx
@@ -1,12 +1,22 @@
 import { HeaderContainer, HeaderContent, NewTransationButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
+
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransationButton>Nova transação</NewTransationButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransationButton>Nova transação</NewTransationButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
