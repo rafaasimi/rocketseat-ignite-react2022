70710ac2a326ee74e2cbb7c46e9a386a00f98e75
Rafael Simionato <rafaasimi@hotmail.com>
2023-06-01 13:25:30 -0300

    Criado funções utilitarias para formatar preço e data

--- projeto_3/03-dtmoney/src/components/Summary/index.tsx
+++ projeto_3/03-dtmoney/src/components/Summary/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
 import { TransactionContext } from "../../contexts/TransactionsContext";
+import { priceFormatter } from '../../utils/formatter';
 
 export function Summary() {
   const { transactions } = useContext(TransactionContext);
@@ -34,7 +35,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e"/>
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -43,7 +44,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68"/>
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -52,7 +53,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff"/>
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
