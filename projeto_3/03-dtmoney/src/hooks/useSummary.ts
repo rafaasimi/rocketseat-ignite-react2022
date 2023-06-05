cd7a70f8f3f61d75f7d0642218d4b9b8c72d9fd7
Rafael Simionato <rafaasimi@hotmail.com>
2023-06-05 09:28:05 -0300

    Add useMemo em calculos complexos para memorizar valores

--- projeto_3/03-dtmoney/src/hooks/useSummary.ts
+++ projeto_3/03-dtmoney/src/hooks/useSummary.ts
@@ -1,5 +1,6 @@
 import { TransactionsContext } from '../contexts/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
+import { useMemo } from 'react'
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
@@ -7,24 +8,26 @@ export function useSummary() {
   })
 
   // { income: 0, outcome: 0, total: 0 }
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
 
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
