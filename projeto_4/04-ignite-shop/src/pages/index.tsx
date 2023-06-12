c7234f16338a55fe86c94818f075ec49ef59d195
Rafael Simionato <rafaasimi@hotmail.com>
2023-06-12 10:54:22 -0300

    Instalação inicial do Stitches

--- projeto_4/04-ignite-shop/src/pages/index.tsx
+++ projeto_4/04-ignite-shop/src/pages/index.tsx
@@ -1,5 +1,26 @@
+import { styled } from '../styles';
+
+export const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+    cursor: 'pointer',
+  }
+});
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
-  )
+    <Button>
+      <span>Teste</span>
+      Enviar
+    </Button>
+  );
 }
