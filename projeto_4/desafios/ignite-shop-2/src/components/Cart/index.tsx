ccd94fc73671555b8453c7ff0fae3134790e97f6
Rafael Simionato <rafaasimi@hotmail.com>
2023-06-26 18:56:05 -0300

    Finalizado estilização do carrinho

--- projeto_4/desafios/ignite-shop-2/src/components/Cart/index.tsx
+++ projeto_4/desafios/ignite-shop-2/src/components/Cart/index.tsx
@@ -1,13 +1,158 @@
-import { Handbag } from "phosphor-react";
-import { BadgeCounter, CartIconContainer } from "./styles";
+import { Handbag, X } from 'phosphor-react';
+import {
+  BadgeCounter,
+  BuyButton,
+  CartIconContainer,
+  CartInfoContainer,
+  CartInfoQuantity,
+  CartInfoTotal,
+  CartItemInfo,
+  CartList,
+  CartListItem,
+  CloseCartButton,
+  ItemImageContainer,
+  ShoppingCartContainer,
+} from './styles';
+import { useState } from 'react';
 
 export default function Cart() {
+  const [isCartActive, setIsCartActive] = useState(false);
+
+  function openAndCloseCart() {
+    setIsCartActive(!isCartActive);
+  }
+
   return (
-    <CartIconContainer>
-      <Handbag size={24} />
-      <BadgeCounter>
-        <span>5</span>
-      </BadgeCounter>
-    </CartIconContainer>
-  )
+    <>
+      <CartIconContainer onClick={openAndCloseCart}>
+        <Handbag size={24} />
+        <BadgeCounter>
+          <span>5</span>
+        </BadgeCounter>
+      </CartIconContainer>
+      <ShoppingCartContainer className={isCartActive ? 'active' : ''}>
+        <CloseCartButton onClick={openAndCloseCart}>
+          <X size={24} weight='bold'/>
+        </CloseCartButton>
+
+        <h3>Sacola de compras</h3>
+
+        <CartList>
+          <CartListItem>
+            <ItemImageContainer>
+              <img src="https://d1wqzb5bdbcre6.cloudfront.net/950aae187b0ba36af225b16b25c29aca2e113116f189b2e071449d2d13c36031/68747470733a2f2f66696c65732e7374726970652e636f6d2f6c696e6b732f4d44423859574e6a64463878546b6c4a536a4a43576e4e776245317a4e32565166475a735833526c63335266617a4a324d3252734d4442734e456b33596e5532647a6b3459323546546a426130306a72416b67347750" alt="" />
+            </ItemImageContainer>
+            <CartItemInfo>
+              <span>Camiseta Beyond the Limits</span>
+              <strong>R$ 79,90</strong>
+              <button>Remover</button>
+            </CartItemInfo>
+          </CartListItem>
+
+          <CartListItem>
+            <ItemImageContainer>
+              <img src="https://d1wqzb5bdbcre6.cloudfront.net/950aae187b0ba36af225b16b25c29aca2e113116f189b2e071449d2d13c36031/68747470733a2f2f66696c65732e7374726970652e636f6d2f6c696e6b732f4d44423859574e6a64463878546b6c4a536a4a43576e4e776245317a4e32565166475a735833526c63335266617a4a324d3252734d4442734e456b33596e5532647a6b3459323546546a426130306a72416b67347750" alt="" />
+            </ItemImageContainer>
+            <CartItemInfo>
+              <span>Camiseta Beyond the Limits</span>
+              <strong>R$ 79,90</strong>
+              <button>Remover</button>
+            </CartItemInfo>
+          </CartListItem>
+
+          <CartListItem>
+            <ItemImageContainer>
+              <img src="https://d1wqzb5bdbcre6.cloudfront.net/950aae187b0ba36af225b16b25c29aca2e113116f189b2e071449d2d13c36031/68747470733a2f2f66696c65732e7374726970652e636f6d2f6c696e6b732f4d44423859574e6a64463878546b6c4a536a4a43576e4e776245317a4e32565166475a735833526c63335266617a4a324d3252734d4442734e456b33596e5532647a6b3459323546546a426130306a72416b67347750" alt="" />
+            </ItemImageContainer>
+            <CartItemInfo>
+              <span>Camiseta Beyond the Limits</span>
+              <strong>R$ 79,90</strong>
+              <button>Remover</button>
+            </CartItemInfo>
+          </CartListItem>
+
+          <CartListItem>
+            <ItemImageContainer>
+              <img src="https://d1wqzb5bdbcre6.cloudfront.net/950aae187b0ba36af225b16b25c29aca2e113116f189b2e071449d2d13c36031/68747470733a2f2f66696c65732e7374726970652e636f6d2f6c696e6b732f4d44423859574e6a64463878546b6c4a536a4a43576e4e776245317a4e32565166475a735833526c63335266617a4a324d3252734d4442734e456b33596e5532647a6b3459323546546a426130306a72416b67347750" alt="" />
+            </ItemImageContainer>
+            <CartItemInfo>
+              <span>Camiseta Beyond the Limits</span>
+              <strong>R$ 79,90</strong>
+              <button>Remover</button>
+            </CartItemInfo>
+          </CartListItem>
+
+          <CartListItem>
+            <ItemImageContainer>
+              <img src="https://d1wqzb5bdbcre6.cloudfront.net/950aae187b0ba36af225b16b25c29aca2e113116f189b2e071449d2d13c36031/68747470733a2f2f66696c65732e7374726970652e636f6d2f6c696e6b732f4d44423859574e6a64463878546b6c4a536a4a43576e4e776245317a4e32565166475a735833526c63335266617a4a324d3252734d4442734e456b33596e5532647a6b3459323546546a426130306a72416b67347750" alt="" />
+            </ItemImageContainer>
+            <CartItemInfo>
+              <span>Camiseta Beyond the Limits</span>
+              <strong>R$ 79,90</strong>
+              <button>Remover</button>
+            </CartItemInfo>
+          </CartListItem>
+
+          <CartListItem>
+            <ItemImageContainer>
+              <img src="https://d1wqzb5bdbcre6.cloudfront.net/950aae187b0ba36af225b16b25c29aca2e113116f189b2e071449d2d13c36031/68747470733a2f2f66696c65732e7374726970652e636f6d2f6c696e6b732f4d44423859574e6a64463878546b6c4a536a4a43576e4e776245317a4e32565166475a735833526c63335266617a4a324d3252734d4442734e456b33596e5532647a6b3459323546546a426130306a72416b67347750" alt="" />
+            </ItemImageContainer>
+            <CartItemInfo>
+              <span>Camiseta Beyond the Limits</span>
+              <strong>R$ 79,90</strong>
+              <button>Remover</button>
+            </CartItemInfo>
+          </CartListItem>
+
+          <CartListItem>
+            <ItemImageContainer>
+              <img src="https://d1wqzb5bdbcre6.cloudfront.net/950aae187b0ba36af225b16b25c29aca2e113116f189b2e071449d2d13c36031/68747470733a2f2f66696c65732e7374726970652e636f6d2f6c696e6b732f4d44423859574e6a64463878546b6c4a536a4a43576e4e776245317a4e32565166475a735833526c63335266617a4a324d3252734d4442734e456b33596e5532647a6b3459323546546a426130306a72416b67347750" alt="" />
+            </ItemImageContainer>
+            <CartItemInfo>
+              <span>Camiseta Beyond the Limits</span>
+              <strong>R$ 79,90</strong>
+              <button>Remover</button>
+            </CartItemInfo>
+          </CartListItem>
+
+          <CartListItem>
+            <ItemImageContainer>
+              <img src="https://d1wqzb5bdbcre6.cloudfront.net/950aae187b0ba36af225b16b25c29aca2e113116f189b2e071449d2d13c36031/68747470733a2f2f66696c65732e7374726970652e636f6d2f6c696e6b732f4d44423859574e6a64463878546b6c4a536a4a43576e4e776245317a4e32565166475a735833526c63335266617a4a324d3252734d4442734e456b33596e5532647a6b3459323546546a426130306a72416b67347750" alt="" />
+            </ItemImageContainer>
+            <CartItemInfo>
+              <span>Camiseta Beyond the Limits</span>
+              <strong>R$ 79,90</strong>
+              <button>Remover</button>
+            </CartItemInfo>
+          </CartListItem>
+
+          <CartListItem>
+            <ItemImageContainer>
+              <img src="https://d1wqzb5bdbcre6.cloudfront.net/950aae187b0ba36af225b16b25c29aca2e113116f189b2e071449d2d13c36031/68747470733a2f2f66696c65732e7374726970652e636f6d2f6c696e6b732f4d44423859574e6a64463878546b6c4a536a4a43576e4e776245317a4e32565166475a735833526c63335266617a4a324d3252734d4442734e456b33596e5532647a6b3459323546546a426130306a72416b67347750" alt="" />
+            </ItemImageContainer>
+            <CartItemInfo>
+              <span>Camiseta Beyond the Limits</span>
+              <strong>R$ 79,90</strong>
+              <button>Remover</button>
+            </CartItemInfo>
+          </CartListItem>
+        </CartList>
+
+        <CartInfoContainer>
+          <CartInfoQuantity>
+            <span>Quantidade</span>
+            <span>3 itens</span>
+          </CartInfoQuantity>
+          <CartInfoTotal>
+            <span>Valor total</span>
+            <strong>R$ 270,00</strong>
+          </CartInfoTotal>
+        </CartInfoContainer>
+
+        <BuyButton>
+          Finalizar compra
+        </BuyButton>
+      </ShoppingCartContainer>
+    </>
+  );
 }
--- projeto_4/desafios/ignite-shop-2/src/components/Cart/styles.ts
+++ projeto_4/desafios/ignite-shop-2/src/components/Cart/styles.ts
@@ -46,3 +46,167 @@ export const BadgeCounter = styled('span', {
     lineHeight: 0,
   }
 })
+
+export const ShoppingCartContainer = styled('aside', {
+  display: 'flex',
+  flexDirection: 'column',
+  
+  position: 'fixed',
+  zIndex: 999,
+  top: 0,
+  right: 0,
+  width: 0,
+  opacity: 0,
+  padding: '1.5rem',
+  maxHeight: '100vh',
+  background: '$gray800',
+  boxShadow: '-4px 0px 30px rgba(0, 0, 0, 0.8)',
+  transition: '0.2s',
+
+  '&.active': {
+    width: '480px',
+    opacity: 1,
+  },
+
+  h3: {
+    margin: '1.5rem 0',
+    color: '$gray100',
+    fontSize: '1.25rem',
+    fontWeight: 'bold',
+  },
+  
+})
+
+export const CloseCartButton = styled('button', {
+  background: 'none',
+  border: 'none',
+  display: 'block',
+  marginLeft: 'auto',
+  
+  svg: {
+    color: '$gray500',
+  },
+
+  '&:hover': {
+    cursor: 'pointer'
+  },
+
+  '&:hover svg': {
+    color: '$gray300',
+  },
+})
+
+export const CartList = styled('ul', {
+  flex: 1,
+  overflowY: 'auto',
+
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.5rem',
+  listStyle: 'none',
+
+  '&::-webkit-scrollbar': {
+    width: '5px',
+  },
+    
+  '&::-webkit-scrollbar-thumb': {
+    borderRadius: '4px',
+    background: '$green500',
+  }
+  
+
+})
+
+export const CartListItem = styled('li', {
+  display: 'flex',
+  gap: '1.25rem',
+
+
+})
+
+export const ItemImageContainer = styled('div', {
+  width: '100px',
+  height: '95px',
+  borderRadius: 8,
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+
+  img: {
+    width: '100%',
+    objectFit: 'cover',
+  }
+})
+
+
+export const CartItemInfo = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+
+  span: {
+    color: '$gray300',
+    fontSize: '1.125rem',
+    marginBottom: 4,
+  },
+
+  strong: {
+    fontSize: '1.125rem',
+    fontWeight: 'bold',
+    color: '$gray100',
+    marginBottom: 8,
+  },
+
+  button: {
+    background: 'none',
+    border: 'none',
+
+    fontSize: '1rem',
+    fontWeight: 'bold',
+    color: '$green500',
+
+    '&:hover': {
+      color: '$green300',
+      cursor: 'pointer',
+    }
+  },
+
+})
+
+export const CartInfoContainer = styled('div', {
+  marginTop: '2rem',
+  marginBottom: '3.5rem',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.5rem',
+})
+
+export const CartInfoQuantity = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+})
+
+export const CartInfoTotal = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  fontWeight: 'bold',
+
+  strong: {
+    fontSize: 24
+  }
+})
+
+export const BuyButton = styled('button', {
+    padding: '1.25rem',
+    color: '$white',
+    backgroundColor: '$green500',
+    border: 'none',
+    borderRadius: 8,
+    fontSize: '1.125rem',
+    marginBottom: '3rem',
+
+    '&:hover': {
+      cursor: 'pointer',
+      backgroundColor: '$green300',
+    }
+})
